0eb87add2025f0f027b946d2e292e957f888bcce
Colleen Murphy <colleen@gazlene.net>
2018-04-28 21:25:54 +0200

    Add beaker tests
    Add functional tests to ensure that the meetbot module can be applied
    without errors, and that the service is accessible. It also validates
    that the vhost template renders as expected, since this tends to be one
    of the harder parts to get right in module development.
    
    Change-Id: I812a7503d0fda4823bbc0bb7b268f8287b2def8b

---
+++ spec/acceptance/basic_spec.rb
@@ -0,0 +1,62 @@
+require 'puppet-openstack_infra_spec_helper/spec_helper_acceptance'
+
+describe 'basic meetbot',  :if => ['debian', 'ubuntu'].include?(os[:family]) do
+  def pp_path
+    base_path = File.dirname(__FILE__)
+    File.join(base_path, 'fixtures')
+  end
+
+  def default_puppet_module
+    module_path = File.join(pp_path, 'default.pp')
+    File.read(module_path)
+  end
+
+  it 'should work with no errors' do
+    apply_manifest(default_puppet_module, catch_failures: true)
+  end
+
+  it 'should be idempotent' do
+    apply_manifest(default_puppet_module, catch_changes: true)
+  end
+
+  describe command(" curl http://localhost") do
+    its(:stdout) { should contain('Welcome to Openstack IRC log server') }
+  end
+
+  expected_vhost = ' # ************************************
+# Managed by Puppet
+# ************************************
+
+NameVirtualHost *:80
+<VirtualHost *:80>
+  ServerName eavesdrop.openstack.org
+  DocumentRoot /srv/meetbot-openstack
+  <FilesMatch \.log$>
+    ForceType text/plain
+    AddDefaultCharset UTF-8
+  </FilesMatch>
+  <Directory /srv/meetbot-openstack>
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride None
+    Order allow,deny
+    allow from all
+    <IfVersion >= 2.4>
+      Require all granted
+    </IfVersion>
+  </Directory>
+
+
+  <Location /alert>
+    Header set Access-Control-Allow-Origin "*"
+  </Location>
+
+
+  ErrorLog /var/log/apache2/eavesdrop.openstack.org_error.log
+  LogLevel warn
+  CustomLog /var/log/apache2/eavesdrop.openstack.org_access.log combined
+  ServerSignature Off
+</VirtualHost>'
+  describe file('/etc/apache2/sites-enabled/50-eavesdrop.openstack.org.conf') do
+    its(:content) { should eq expected_vhost }
+  end
+end
