e9cef8df0f555baff006205f6339df2cf51a499d
Colleen Murphy <colleen@gazlene.net>
2018-04-28 21:25:54 +0200

    Add Zuulv3 functional tests
    Add a manifest fixture and tests to ensure that the meetbot module can
    be applied without errors, and that the service is accessible. It also
    validates that the vhost template renders as expected, since this tends
    to be one of the harder parts to get right in module development.
    
    Depends-On: https://review.openstack.org/565270
    
    Change-Id: I812a7503d0fda4823bbc0bb7b268f8287b2def8b

---
+++ tests/scenarios/default/manifest.pp
@@ -0,0 +1,18 @@
+include httpd
+include meetbot
+$vhost_extra = '
+<Location /alert>
+  Header set Access-Control-Allow-Origin "*"
+</Location>
+'
+meetbot::site { 'openstack':
+  nick         => 'openstack',
+  nickpass     => 'nickpass',
+  network      => 'FreeNode',
+  server       => 'chat.freenode.net:7000',
+  use_ssl      => 'True',
+  vhost_extra  => $vhost_extra,
+  vhost_name   => 'eavesdrop.openstack.org',
+  manage_index => true,
+  channels     => ['#one', '#two', '#three'],
+}
---
+++ tests/scenarios/default/tests.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -eux
+
+test_site_is_alive() {
+    curl http://localhost | grep 'Welcome to Openstack IRC log server foobar' || return 1
+    return 0
+}
+
+test_vhost_content() {
+    expected_vhost=$(mktemp)
+    cat <<EOF > $expected_vhost
+# ************************************
+# Managed by Puppet
+# ************************************
+
+NameVirtualHost *:80
+<VirtualHost *:80>
+  ServerName eavesdrop.openstack.org
+ ServerAlias
+  DocumentRoot /srv/meetbot-openstack
+  <FilesMatch \.log$>
+    ForceType text/plain
+    AddDefaultCharset UTF-8
+  </FilesMatch>
+  <Directory /srv/meetbot-openstack>
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride None
+    Order allow,deny
+    allow from all
+    <IfVersion >= 2.4>
+      Require all granted
+    </IfVersion>
+  </Directory>
+
+
+  <Location /alert>
+    Header set Access-Control-Allow-Origin "*"
+  </Location>
+
+
+  ErrorLog /var/log/apache2/eavesdrop.openstack.org_error.log
+  LogLevel warn
+  CustomLog /var/log/apache2/eavesdrop.openstack.org_access.log combined
+  ServerSignature Off
+</VirtualHost>
+EOF
+    diff $expected_vhost /etc/apache2/sites-enabled/50-eavesdrop.openstack.org.conf || return 1
+    return 0
+}
+
+declare -a tests
+tests=(
+    test_site_is_alive
+     test_vhost_content
+)
+for test in $tests \
+; do
+    $test || exit 1
+done
+
+exit 0
